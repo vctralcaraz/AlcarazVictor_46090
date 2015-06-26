a48365203cf80ba216f5d97ab11f866de44ce08c
vctralcaraz <vctralcaraz@gmail.com>
2015-06-25 18:13:42 -0700

    Signed-off-by: vctralcaraz <vctralcaraz@gmail.com>

---
+++ Template/main.cpp
@@ -0,0 +1,26 @@
+
+//System Libraries
+#include <iostream>//I/O Library
+using namespace std;//Standard namespace for iostream
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    
+    //Declare Variables
+    
+    //Input Variables
+    
+    //Calculate process
+    
+    //Output
+    
+    //Exit Stage Right!
+    return 0;
+}
+
